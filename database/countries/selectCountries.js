d6ecfaceeff311109da25fbf7db3abf26ccf1ac8
Robert <zuiohi@gmail.com>
2018-08-20 16:46:42 +0300

    Added country url names to get countries api call data

--- database/countries/selectCountries.js
+++ database/countries/selectCountries.js
@@ -5,7 +5,8 @@ module.exports = {
     return db.any(
       `
         SELECT country.id AS country_id,
-               translated_country.name AS country_name
+               translated_country.name AS country_name,
+               translated_country.url_name AS country_url_name
         FROM country
         JOIN translated_country ON (
           translated_country.country_id = country.id
